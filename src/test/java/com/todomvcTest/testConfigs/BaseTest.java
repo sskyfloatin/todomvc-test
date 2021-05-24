7e678cbe2881835051d098b39ded6d8692f31545
Roman Tytarenko <roman.tytarenko@macc.com.ua>
2021-05-24 20:45:03 +0300

    add BaseTest, task 'tests-independence-day-md'.

--- src/test/java/com/todomvcTest/testConfigs/BaseTest.java
+++ src/test/java/com/todomvcTest/testConfigs/BaseTest.java
@@ -2,7 +2,17 @@ package com.todomvcTest.testConfigs;
 
 import com.codeborne.selenide.Configuration;
 
+import static com.codeborne.selenide.Selenide.Wait;
+import static com.codeborne.selenide.Selenide.open;
+import static org.openqa.selenium.support.ui.ExpectedConditions.jsReturnsValue;
+
 public class BaseTest {
+
+    public void openApp() {
+        open(Configuration.baseUrl);
+        Wait().until(jsReturnsValue
+                ("return $._data($('#clear-completed').get(0), 'events').hasOwnProperty('click')"));
+    }
 
     {
         Configuration.fastSetValue = true;
